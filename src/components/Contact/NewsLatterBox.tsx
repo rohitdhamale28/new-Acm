350d4817082724330752203d6e058ce8740167b7
Rohit Dhamale <rohitdhamale05@gmail.com>
2024-09-21 20:24:13 +0530

    new-Acm

--- src/components/Contact/NewsLatterBox.tsx
+++ src/components/Contact/NewsLatterBox.tsx
@@ -8,11 +8,10 @@ const NewsLatterBox = () => {
   return (
     <div className="relative z-10 rounded-sm bg-white p-8 shadow-three dark:bg-gray-dark sm:p-11 lg:p-8 xl:p-11">
       <h3 className="mb-4 text-2xl font-bold leading-tight text-black dark:text-white">
-        Subscribe to receive future updates
+        Register to receive future updates From ACM !
       </h3>
       <p className="mb-11 border-b border-body-color border-opacity-25 pb-11 text-base leading-relaxed text-body-color dark:border-white dark:border-opacity-25">
-        Lorem ipsum dolor sited Sed ullam corper consectur adipiscing Mae ornare
-        massa quis lectus.
+        We will keep you updated about the upcoming Events, Opportunities and guide you throughout your journey and  !
       </p>
       <div>
         <input
